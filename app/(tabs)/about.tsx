c046f197bc31fd8c2f5d57b546fe2f3e1120e7a5
Franz Wilder <gualambo@gmail.com>
2025-01-01 21:58:22 -0500

    tabs select photo

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Esto es el about</Text>
+   
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  }
+});
